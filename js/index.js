1fa8e46ae080f0ef419585a1ba8f940bb59b2360
timezhong <timezhon@gmail.com>
2018-11-22 18:08:07 +0800

    优化img标签转化和拖动体验

--- js/index.js
+++ js/index.js
@@ -10,20 +10,30 @@ var dragDrop = require('drag-drop');
 var $ = require('jQuery');
 var rem  = require('remove-html-element');
 
-
 const holder = document.getElementById('dropTarget')
-    holder.ondragover = () => {
+    holder.ondragover = (e) => {
+        //拖入和成功状态
+        document.getElementById('dropTarget').classList.add('act');
+        document.getElementById('dropTarget').classList.remove('succ');
         return false;
+        
     }
-    holder.ondragleave = holder.ondragend = () => {
+    holder.ondragleave = holder.ondragend = (e) => {
+        //拖入和成功状态
+        document.getElementById('dropTarget').classList.remove('act');
+        document.getElementById('dropTarget').classList.remove('succ');
         return false;
     }
     holder.ondrop = (e) => {
+        //拖入和成功状态
+        document.getElementById('dropTarget').classList.remove('act');
+        document.getElementById('dropTarget').classList.remove('succ');
         e.preventDefault()
         for (let f of e.dataTransfer.files) {
         console.log('File(s) you dragged here: ', f.path);
         var wxmlPath = path.dirname(f.path)+'/wxml/'; //在当前目录新建一个wxml文件夹 注意最后
         var wxmlFilePath = wxmlPath+'*'; 
+
         //创建目录
         if (!fs.existsSync(wxmlPath)){
             fs.mkdirSync(wxmlPath);
@@ -34,105 +44,118 @@ const holder = document.getElementById('dropTarget')
         });
 
         setTimeout(function(){
-        //替换标签
-        glob(wxmlFilePath, {nodir: true}, function (err, files) {
-            if (err) {
-                return console.error("替换标签失败");
-            }
-            files.forEach( function (file){
-                //head标签内内容
-                var regex = /<![\s\S]*?<\/head>/;
-                fs.readFile(file, 'utf8', function (err,data) {
-                    if (err) {
-                        return console.log(err);
-                    }
-                    let m;
-                    if ((m = regex.exec(data)) !== null) {
-                        m.forEach((match, groupIndex) => {
-                            var result = data.replace(`${match}`, '');
-                            fs.writeFile(file, result, 'utf8', function (err) {
-                             if (err) return console.log(err);
+            //替换标签
+            glob(wxmlFilePath, {nodir: true}, function (err, files) {
+                if (err) {
+                    return console.error("替换标签失败");
+                }
+                files.forEach( function (file){
+
+                    //head标签内内容
+                    var regex = /<![\s\S]*?<\/head>/;
+                    fs.readFile(file, 'utf8', function (err,data) {
+                        if (err) {
+                            return console.log(err);
+                        }
+                        let m;
+                        if ((m = regex.exec(data)) !== null) {
+                            m.forEach((match, groupIndex) => {
+                                var result = data.replace(`${match}`, '');
+                                fs.writeFile(file, result, 'utf8', function (err) {
+                                 if (err) return console.log(err);
+                                });
                             });
-                        });
-                    }
-                });
-                //view
-                replace({
-                    regex: "(<body|<article|<aside|<ul|<li|<ol|<caption|<dd|<dl|<dt|<footer|<header|<section|<table|<thead|<tbody|<tr|<td|<th|<h1|<h2|<h3|<h4|<h5|<h6|<div|<p)",
-                    replacement: "<view",
-                    paths: [file],
-                });
-                //html
-                 replace({
-                    regex: "(</html>)",
-                    replacement: "",
-                    paths: [file],
-                 });
+                        }
+                    });
+                    //view
+                    replace({
+                        regex: "(<body|<article|<aside|<ul|<li|<ol|<caption|<dd|<dl|<dt|<footer|<header|<section|<table|<thead|<tbody|<tr|<td|<th|<h1|<h2|<h3|<h4|<h5|<h6|<div|<p)",
+                        replacement: "<view",
+                        paths: [file],
+                    });
+                    //html
+                     replace({
+                        regex: "(</html>)",
+                        replacement: "",
+                        paths: [file],
+                     });
+
+                    //view 
+                     replace({
+                        regex: "(</body>|</article>|</aside>|</ul>|</li>|</ol>|</caption>|</dd>|</dl>|</dt>|</footer>|</header>|</nav>|</section>|</table>|</thead>|</tbody>|</tr>|</td>|</th>|</h1>|</h2>|</h3>|</h4>|</h5>|</h6>|</div>|</p>)",
+                        replacement: "</view>",
+                        paths: [file],
+                     });
 
 
-                 replace({
-                    regex: "(</body>|</article>|</aside>|</ul>|</li>|</ol>|</caption>|</dd>|</dl>|</dt>|</footer>|</header>|</nav>|</section>|</table>|</thead>|</tbody>|</tr>|</td>|</th>|</h1>|</h2>|</h3>|</h4>|</h5>|</h6>|</div>|</p>)",
-                    replacement: "</view>",
-                    paths: [file],
-                 });
+                     // img标签
+                     replace({
+                        regex: '(<img.*?\>)',
+                        replacement: '$1</image>',
+                        paths: [file],
+                     });
+                     // image标签增加属性
+                     replace({
+                        regex: "(<img)",
+                        replacement: '<image mode=""',
+                        paths: [file],
+                     });
+                   //nav
+                     replace({
+                        regex: "(<nav>)",
+                        replacement: "<view>",
+                        paths: [file],
+                     });
+                    //nav
+                     replace({
+                        regex: "(<nav )",
+                        replacement: "<view ",
+                        paths: [file],
+                     });
+                     //text
+                     replace({
+                        regex: "(<i>|<s>|<b>)",
+                        replacement: "<text>",
+                        paths: [file],
+                     });
+                     //text
+                     replace({
+                        regex: "(<i |<s |<b )",
+                        replacement: "<text ",
+                        paths: [file],
+                     });
+                     //text
+                     replace({
+                        regex: "(<span|<strong)",
+                        replacement: "<text",
+                        paths: [file],
+                     });
+                     replace({
+                        regex: "(</span>|</s>|</b>|</i>|</strong>)",
+                        replacement: "</text>",
+                        paths: [file],
+                     });
 
-                //image
-                 replace({
-                    regex: "(<img)",
-                    replacement: "<image",
-                    paths: [file],
-                 });
-               //image
-                 replace({
-                    regex: "(<nav>)",
-                    replacement: "<view>",
-                    paths: [file],
-                 });
-                //image
-                 replace({
-                    regex: "(<nav )",
-                    replacement: "<view ",
-                    paths: [file],
-                 });
-                 //text
-                 replace({
-                    regex: "(<i>|<s>|<b>)",
-                    replacement: "<text>",
-                    paths: [file],
-                 });
-                 //text
-                 replace({
-                    regex: "(<i |<s |<b )",
-                    replacement: "<text ",
-                    paths: [file],
-                 });
-                 //text
-                 replace({
-                    regex: "(<span|<strong)",
-                    replacement: "<text",
-                    paths: [file],
-                 });
-                 replace({
-                    regex: "(</span>|</s>|</b>|</i>|</strong>)",
-                    replacement: "</text>",
-                    paths: [file],
-                 });
+                     //navigator
+                     replace({
+                        regex: "(<a)",
+                        replacement: "<navigator",
+                        paths: [file],
+                     });
+                     replace({
+                        regex: "(</a>)",
+                        replacement: "</navigator>",
+                        paths: [file],
+                     });
+          });
+        }) 
+        //替换标签  
+        },2000);
+        
+        // 成功状态
+        document.getElementById('dropTarget').classList.add('succ');
 
-                 //navigator
-                 replace({
-                    regex: "(<a)",
-                    replacement: "<navigator",
-                    paths: [file],
-                 });
-                 replace({
-                    regex: "(</a>)",
-                    replacement: "</navigator>",
-                    paths: [file],
-                 });
-      });
-    }) 
-    //替换标签  
-    },2000);
+
     }
     return false;
 }
